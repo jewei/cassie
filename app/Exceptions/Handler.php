5f0259fce4f78a22a971639bb21ee94203156e98
Jewei Mak <jewei.mak@gmail.com>
2023-02-11 19:42:15 +0800

    feat: Custom error pages

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,7 +3,10 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
-use Throwable;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Inertia\Response as InertiaResponse;
+use Symfony\Component\HttpFoundation\Response as SymfonyResponse;
 
 class Handler extends ExceptionHandler
 {
@@ -13,7 +16,7 @@ class Handler extends ExceptionHandler
      * @var array<class-string<\Throwable>, \Psr\Log\LogLevel::*>
      */
     protected $levels = [
-        //
+
     ];
 
     /**
@@ -22,7 +25,7 @@ class Handler extends ExceptionHandler
      * @var array<int, class-string<\Throwable>>
      */
     protected $dontReport = [
-        //
+
     ];
 
     /**
@@ -41,8 +44,50 @@ class Handler extends ExceptionHandler
      */
     public function register(): void
     {
-        $this->reportable(function (Throwable $e) {
-            //
+        $this->reportable(function (\Throwable $e) {
         });
+    }
+
+    /**
+     * Render an exception into an HTTP response.
+     *
+     * @param  Request  $request
+     * @return InertiaResponse|SymfonyResponse
+     */
+    public function render($request, \Throwable $e)
+    {
+        /** @var \Illuminate\Http\Response $response */
+        $response = parent::render($request, $e);
+        $status = $response->status();
+
+        return ! in_array($status, [500, 503, 404, 403])
+            ? $response
+            : Inertia::render('Error/Error', [
+                'status' => $status,
+                'statusText' => $this->shortErrorMessage($status),
+                'message' => $this->friendlyErrorMessages($status),
+            ]);
+    }
+
+    private function shortErrorMessage(int $code): string
+    {
+        return match ($code) {
+            403 => 'Resource is forbidden',
+            404 => 'Page not found',
+            500 => 'Server-side error',
+            503 => 'Service unavailable',
+            default => 'Whoops!',
+        };
+    }
+
+    private function friendlyErrorMessages(int $code): string
+    {
+        return match ($code) {
+            403 => 'Whoops! It looks like you don’t have permission to access this page. If you think this is a mistake, please contact the website administrator for assistance.',
+            404 => 'Oops! We couldn’t find the page you were looking for. It might have been moved, deleted, or simply unavailable. Don’t worry though, there are many other pages on this website that might interest you.',
+            500 => 'Whoops! Something went wrong on our end. We’re working on fixing it as quickly as we can. Please try again later or contact our support team if the problem persists.',
+            503 => 'Sorry! The website is currently undergoing some maintenance or is experiencing high traffic. We’re working hard to get things back up and running as soon as possible. Please check back later.',
+            default => 'Something went wrong',
+        };
     }
 }
---
+++ resources/js/Pages/Error/Error.vue
@@ -0,0 +1,28 @@
+<script setup>
+import ErrorLayout from '@/Layouts/ErrorLayout.vue';
+import { Link } from '@inertiajs/vue3';
+
+defineProps({
+    status: Number,
+    statusText: String,
+    message: String,
+});
+
+</script>
+<template>
+    <ErrorLayout>
+        <main class="isolate min-h-full">
+            <img src="https://images.unsplash.com/photo-1545972154-9bb223aac798?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3050&q=80&exp=8&con=-15&sat=-75"
+                alt="" class="absolute inset-0 -z-10 h-full w-full object-cover object-top" />
+            <div class="mx-auto max-w-2xl px-6 py-32 text-center sm:py-40 lg:px-8">
+                <p class="text-base font-semibold leading-8 text-white">{{ status }}</p>
+                <h1 class="mt-4 text-3xl font-bold tracking-tight text-white sm:text-5xl">{{ statusText }}</h1>
+                <p class="mt-4 text-base text-white/70 sm:mt-6">{{ message }}</p>
+                <div class="mt-10 flex justify-center">
+                    <Link :href="route('templates.index')" class="text-sm font-semibold leading-7 text-white"><span aria-hidden="true">&larr;</span>
+                        Back to home</Link>
+                </div>
+            </div>
+        </main>
+    </ErrorLayout>
+</template>
