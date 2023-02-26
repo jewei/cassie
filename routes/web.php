fbe97543ce085454797c31952d8a9258cd77c7a6
Jewei Mak <jewei.mak@gmail.com>
2023-02-26 22:15:46 +0800

    refactor: Split routes

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,6 @@
 <?php
 
-use App\Http\Controllers\CertificateController;
-use App\Http\Controllers\ExportController;
-use App\Http\Controllers\FontController;
-use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\SearchCertificateController;
-use App\Http\Controllers\SettingController;
-use App\Http\Controllers\TemplateController;
 use App\Models\Certificate;
 use Illuminate\Support\Facades\Route;
 
@@ -28,42 +22,3 @@ Route::controller(SearchCertificateController::class)
         Route::get('/', 'index')->name('index');
         Route::post('/', 'search')->name('search');
     });
-
-/**
- * Admin Dashboard.
- */
-Route::middleware('auth')->group(function () {
-    Route::post('templates/{template}/regenerate', [TemplateController::class, 'regenerate'])->name('templates.regenerate');
-    Route::resource('templates', TemplateController::class)->except('show');
-
-    Route::resource('templates.certificates', CertificateController::class)->only(['index', 'show', 'create', 'store']);
-
-    Route::get('settings', [SettingController::class, 'index'])->name('settings.index');
-
-    Route::controller(ExportController::class)
-        ->prefix('exports')
-        ->name('exports.')
-        ->group(function () {
-            Route::get('{template}/participants', 'participants')->name('participants');
-            Route::get('{template}/certificates', 'certificates')->name('certificates');
-        });
-
-    Route::controller(FontController::class)
-        ->prefix('fonts')
-        ->name('fonts.')
-        ->group(function () {
-            Route::post('/', 'store')->name('store');
-            Route::delete('/', 'destroy')->name('destroy');
-        });
-
-    Route::controller(ProfileController::class)
-        ->prefix('profile')
-        ->name('profile.')
-        ->group(function () {
-            Route::get('/', 'edit')->name('edit');
-            Route::patch('/', 'update')->name('update');
-            Route::delete('/', 'destroy')->name('destroy');
-        });
-});
-
-require __DIR__.'/auth.php';
